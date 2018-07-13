2c0d1baeef7083f908141b9e54b7fad93207c4f5
Hisato Kawaminami <hisatokawaminami@gmail.com>
2018-07-13 15:08:04 -0700

    add home index, stylist index, form pages

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+    [HttpGet("/stylists")]
+    public ActionResult Index()
+    {
+      return View(Stylist.GetAll());
+    }
+    [HttpGet("/stylists/add")]
+    public ActionResult CreateForm()
+    {
+      return View(Stylist.GetAll());
+    }
+    [HttpPost("/stylists")]
+    public ActionResult CollectInfo(string newstylist)
+    {
+      Stylist newStylist = new Stylist(newstylist);
+      newStylist.Save();
+      // List<Item> all = Item.GetAll();
+      return RedirectToAction("Index");
+    }
+  }
+}
